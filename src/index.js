40740b5c4f2f8d2848b5d0260b192a8f09b19ac0
StepbyStepper <gorbunov.dv.webdev@gmail.com>
2018-11-25 16:25:35 +0300

    Implementing time travel

--- src/index.js
+++ src/index.js
@@ -45,15 +45,18 @@ class Game extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			history: [{
-				squares: Array(9).fill(null),
-			}],
-			xIsNext: true,
-		}
+			history: [
+				{
+					squares: Array(9).fill(null)
+				}
+			],
+			stepNumber: 0,
+			xIsNext: true
+		};
 	}
 
 	handleClick(i) {
-		const history = this.state.history;
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
 		const current = history[history.length - 1];
 		const squares = current.squares.slice();
 		if (calculateWinner(squares) || squares[i]) {
@@ -61,16 +64,26 @@ class Game extends React.Component {
 		}
 		squares[i] = this.state.xIsNext ? 'X': 'O';
 		this.setState({
-			history: history.concat([{
-				squares: squares
-			}]),
+			history: history.concat([
+				{
+					squares: squares
+				}
+			]),
+			stepNumber: history.length,
 			xIsNext: !this.state.xIsNext,
+		});
+	}
+
+	jumpTo(step) {
+		this.setState({
+			stepNumber: step,
+			xIsNext: (step % 2) === 0
 		});
 	}
 
 	render() {
 		const history = this.state.history;
-		const current = history[history.length -1];
+		const current = history[this.state.stepNumber];
 		const winner = calculateWinner(current.squares);
 
 		const moves = history.map((step, move) => {
@@ -78,7 +91,7 @@ class Game extends React.Component {
 				'Go to move #' + move :
 				'Go to game start';
 				return (
-					<li>
+					<li key={move}>
 						<button onClick={() => this.jumpTo(move)}>{desc}</button>
 					</li>
 				)
@@ -96,17 +109,24 @@ class Game extends React.Component {
 				<div className="game-board">
 					<Board 
 						squares={current.squares}
-						onClick={(i) => this.handleClick(i)}
+						onClick={i => this.handleClick(i)}
 					/>
 				</div>
 				<div className="game-info">
 					<div>{status}</div>
-					<ol>{/* TODO */}</ol>
+					<ol>{moves}</ol>
 				</div>
 			</div>
 		);
 	}
 }
+
+// ========================================
+
+ReactDOM.render(
+	<Game />,
+	document.getElementById('root')
+);
 
 function calculateWinner(squares) {
 	const lines = [
@@ -129,10 +149,3 @@ function calculateWinner(squares) {
 
 	return null;
 }
-
-// ========================================
-
-ReactDOM.render(
-	<Game />,
-	document.getElementById('root')
-);
